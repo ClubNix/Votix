f57b6aae5662d2581c6228d389cef0ab2f478048
Philippe Lewin <philippe.lewin@gmail.com>
2020-08-03 11:57:50 +0200

    UI to verify results

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -8,9 +8,13 @@
  */
 namespace App\Controller;
 
+use App\Service\VoteCounterServiceInterface;
+use App\Repository\CandidateRepository;
 use App\Repository\VoterRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
@@ -18,6 +22,13 @@ use Symfony\Component\Routing\Annotation\Route;
  */
 class AdminController extends AbstractController
 {
+    private $secret;
+
+    public function __construct(string $secret)
+    {
+        $this->secret = $secret;
+    }
+
     /**
      * @Route("/admin/voters-list", name="admin_voters")
      *
@@ -31,6 +42,59 @@ class AdminController extends AbstractController
 
         return $this->render('default/admin-voters-list.html.twig', [
             'voters' => $voters,
+        ]);
+    }
+
+    /**
+     * @Route("/admin/verify-results-hash", name="admin_verify_results_hash")
+     *
+     * @param Request $request
+     * @param VoteCounterServiceInterface $counterService
+     * @param CandidateRepository $candidateRepository
+     *
+     * @return Response
+     */
+    public function verifyResultsHash(Request $request, VoteCounterServiceInterface $counterService, CandidateRepository $candidateRepository): Response
+    {
+        $isChecksumValid = null;
+
+        if ($request->request->has('checksum')) {
+            /** @var Candidate[] $candidates */
+            $candidates = $candidateRepository->findAll();
+            $candidatesIndexed = [];
+            foreach ($candidates as $candidate) {
+                $candidatesIndexed[$candidate->getId()] = $candidate;
+            }
+
+            $results = [];
+            foreach ($request->request as $key => $value) {
+                if (substr($key, 0, 6 ) !== "count-") {
+                   continue;
+                }
+
+                $key = explode('-', $key)[1];
+
+                if (!array_key_exists($key, $candidatesIndexed)) {
+                    throw new BadRequestHttpException();
+                }
+
+                $results[$key] = [
+                    'candidate' => $candidatesIndexed[$key],
+                    'count' => $value,
+                ];
+            }
+
+            $hash = $counterService->hashResults($results, $this->secret);
+
+            $isChecksumValid = hash_equals($hash, $request->request->get('checksum'));
+        }
+
+        /** @var Candidate[] $candidates */
+        $candidates = $candidateRepository->findAllShuffled();
+
+        return $this->render('default/admin-verify-results-hash.html.twig', [
+            'candidates' => $candidates,
+            'isChecksumValid' => $isChecksumValid,
         ]);
     }
 }
